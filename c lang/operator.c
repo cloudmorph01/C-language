4bc5495ed53bf0ab113600f3962a12863199c9b7
Ravi yadav <ry678279@gmail.com>
2026-09-20 22:46:23 +0530

    Add files via upload

---
+++ c lang/operator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+   int a= 4==4;
+   // 4==4 is true so it will return 1
+   printf("%d",a);
+   //== means what is 4 equals to 4 or not if yes then it will return 1 otherwise 0
+
+   return 0;
+} 
