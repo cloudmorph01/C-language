3675c2da610605f756de6af1088e1035f14ea0cf
Ravi yadav <ry678279@gmail.com>
2026-09-20 22:49:04 +0530

    Add files via upload

---
+++ c lang/sumoftwonumber.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    float a;
+    printf("Enter the first number: ");
+    scanf("%f",&a);
+    float b;
+    printf("Enter the second number: ");
+    scanf("%f",&b);
+    printf("the sum:%f",a + b);
+    return 0;
+}
