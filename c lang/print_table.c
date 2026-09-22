e179fb30e2bc382c9089c11d5a1c84f7d13872d2
Ravi yadav <ry678279@gmail.com>
2026-09-22 22:49:55 +0530

    Add files via upload

---
+++ c lang/print_table.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int number;
+    int i;
+    printf("Enter your number: ");
+    scanf("%d",&number);
+    for(i=1;i<=10;i++){
+        printf("%d * %d = %d\n", number, i, number * i);
+    }
+    return 0;
+}
