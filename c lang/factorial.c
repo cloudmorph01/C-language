33d1e70c6772dbeb5a2f2da429de2079dc09f71e
Ravi yadav <ry678279@gmail.com>
2026-09-22 22:49:11 +0530

    Add files via upload

---
+++ c lang/factorial.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    factorial(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        else{
+            return n*factorial(n-1);
+        }
+    }
+    return 0;
+}
